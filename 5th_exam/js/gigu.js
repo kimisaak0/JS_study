09a7158b97c00eb75200202cf0566ca186b047d2
김이삭 <kimisaak0@gmail.com>
2022-01-28 02:12:19 +0900

    27일 밤

--- 5th_exam/js/gigu.js
+++ 5th_exam/js/gigu.js
@@ -1,35 +1,29 @@
-//전역변수로 선언
 var $gigu = null;
 
 $(document).ready(function() {
-    initialize();
-    event_gigu();            
+    init_zigu();
+    event_zigu();
 });
-//전역변수를 초기화
-function initialize() {
-    //지구본 찾기
+
+function init_zigu() {
     $gigu = $("#gigu");
 }
-//이벤트와 관련된 내용을 함수로 등록
-function event_gigu() {
-    //버튼에 이벤트 등록
+
+function event_zigu() {
     $("#btnStart").click(function(){
-        //지구본 움직이기
-        //지구본 위치 값 구하기
         var x = parseInt($("#txtX").val());
         var y = parseInt($("#txtY").val());
-        moveGigu(x, y);
+        moveZigu(x,y);
     });
 }
-//지구본의 움직이는 기능을 구현해놓은 함수
-function moveGigu(x, y){
-    if((x >= 0 && x <= 500) && (y >= 0 && y <= 300)){
-            $gigu.css({
-                left : x,
-                top : y
-            });
-    }
-    else {
-        alert("입력된 값이 범위를 벗어났습니다.");
+
+function moveZigu(x,y) {
+    if((x >= 0 && x <= 500)  && (y >= 0 && y <= 300) ) {
+        $gigu.css({
+            left: x,
+            top: y
+        });
+    } else {
+        alert("입력된 값이 범위를 벗어났습니다.")
     }
 }
